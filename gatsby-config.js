15a8ae18037818bc7201e9f56d50214ad701ad14
Jimmy Zhang <imyjimmy@gmail.com>
2019-01-02 22:32:44 -0500

    trying to bring in concepts from https://github.com/gatsbyjs/gatsby-starter-blog
    basically tring to incorporate parts of the blog without crashing the cara template.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,47 @@ module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
     siteUrl: config.siteUrl + pathPrefix,
+    title: `siteMetadata.title`,
+    author: `Jimmy Fan Zhang`
   },
   /* Plugins */
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-emotion',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/images/`,
-        name: 'images',
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/comedyBlog`,
+        name: `comedyBlog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     },
     'gatsby-transformer-sharp',
---
+++ gatsby-config.stub.js
@@ -0,0 +1,7 @@
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/src/images/`,
+    //     name: 'images',
+    //   },
+    // },
