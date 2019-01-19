212b330690e1dc8340d43d04fed63ccf5c012e0e
Jimmy Zhang <imyjimmy@gmail.com>
2019-01-19 14:27:54 -0500

    changed site config such as bg, site title, description etc

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'imyjimmy - Code & Comedy', // Navigation and Site Title
+  siteTitleAlt: 'imyjimmy', // Alternative Site title for SEO
+  siteUrl: 'https://imyjimmy.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with big typography', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'imyjimmy Code & Comedy', // Your site description
+  author: '@imyjimmy', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
