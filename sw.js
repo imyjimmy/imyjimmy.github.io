660b1de04e6f8f26bed9f24a2fa11cd7ceeca493
Jimmy Zhang <imyjimmy@gmail.com>
2019-02-17 18:24:07 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6fce0315eb4d28327dec.js"
+    "url": "webpack-runtime-b33f4b71e23f52229135.js"
   },
   {
     "url": "app-cb8be212306bf6a47bd4.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "350341641aeef72003f26297cd6ec429"
+    "revision": "cca6d4b40bf4bdb470c47911975cdc3b"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
