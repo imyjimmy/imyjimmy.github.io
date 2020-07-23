489241e7410ae1df364bcdadd49ac70b7be45373
Jimmy Zhang <jizhang@athenahealth.com>
2020-07-22 21:35:49 -0400

    added postcss and tailwind.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
-};
+  plugins: ['tailwindcss']
+}
+
+// module.exports = {
+//   plugins: ["tailwindcss", "postcss-preset-env"],
+// };
+
+// const tailwindcss = require('tailwindcss');
+
+// module.exports = {
+//   plugins: [require('tailwindcss')], or //  plugins: [tailwindcss('./tailwind.js')],
+
+// };
+
