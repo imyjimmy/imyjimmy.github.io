b957f68a4ad4a946628b8aa1312b8803a3c85d9f
Jimmy Zhang <imyjimmy@gmail.com>
2023-04-09 16:24:22 -0700

    more btc regrets progress

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -50,17 +50,17 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
+              {/* <Image
                 src={portraitImage}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-              />
+              /> */}
             </div>
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Jimmy Fan Zhang. .
+              I’m Jimmy Fan Zhang.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
@@ -80,13 +80,13 @@ export default function About() {
               <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
                 LinkedIn
               </SocialLink>
-              <SocialLink
+              {/* <SocialLink
                 href="mailto:imyjimmy@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
                 imyjimmy@gmail.com
-              </SocialLink>
+              </SocialLink> */}
             </ul>
           </div>
         </div>
--- src/pages/demos/bitcoinregrets.jsx
+++ src/pages/demos/bitcoinregrets.jsx
@@ -1,26 +1,74 @@
+import { useState, Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+
 import { SimpleLayout } from '@/components/SimpleLayout'
+
+// import MonthSelector from '@/components/MonthSelector';
+// import YearSelector from '@/components/YearSelector';
 
 import * as d3 from "d3";
 import fetch from "isomorphic-unfetch";
 
 export async function getServerSideProps() {
-  const response = await fetch(
-    "https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-01-01&end=2023-03-01&index=USD&interval=monthly"
-  );
-  const { bpi } = await response.json();
-  const btcPrice = Object.entries(bpi).map(([date, price]) => ({
-    x: date,
-    y: price,
-  }));
+  const today = Date.now(); //unix time stamp of now
+  
+  /* Jan 1, 2013: 1357027200
+    Jan 1, 2022: 1641024000
+  */
+  
+  console.log('time: ', today)
+  const response = await fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1641024000&to=${today}`);
+  const { prices } = await response.json();
+
+  const btcPrice = Object.entries(prices).map(([,[date, price]]) => { 
+    return ({
+      x: date,
+      y: price,
+    })});
   return { props: { btcPrice } };
 }
 
+const getUnixTime = (month,year) => {
+  let x = new Date(`${year}-${month}-01`).valueOf()
+  return x;
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const bitcoinregrets = ({ btcPrice }) => {
+  const [usd, setUsd] = useState()
+  /* set up years and months */
+  const [month, setMonth] = useState()
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const [year, setYear] = useState()
+  const years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
+
+  const handleInputAmt = (event) => {
+    event.preventDefault()
+    console.log('event:', event.target.value)
+    setUsd(event.target.value)
+  }
+
+  const handleSelectMonth = (month) => (event) => {
+    console.log('select:', event, event.target, event.target.value, month)
+    event.preventDefault()
+    setMonth(month)
+  }
+
+  const handleSelectYear = (year) => (event) => {
+    console.log('select:', event, event.target, event.target.value, year)
+    event.preventDefault()
+    setYear(year)
+  }
+
   const margin = { top: 20, right: 20, bottom: 30, left: 60 };
   const width = 960 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
-  const parseDate = d3.timeFormat("%Y-%m-%d");
+  // const parseDate = d3.timeFormat("%Y-%m-%d");
 
   const x = d3
     .scaleTime()
@@ -48,10 +96,90 @@ const bitcoinregrets = ({ btcPrice }) => {
       >
     <div id="hey" className="w-full bg-white dark:text-zinc-400 dark:bg-zinc-900 dark:ring-zinc-300/20">
       {/* { console.log('btcPrice:', btcPrice, 'line(btcPrice): ', line(btcPrice))} */}
+      {/* <div>{usd},{month},{year}</div> */}
+      <h2 className="text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-6xl">I wish I bought $<input onChange={handleInputAmt} className="w-1/6 bg-black dark:bg-white text-zinc-900"></input></h2>
+      <h3 className="mt-4 dark:text-zinc-100 sm:text-5xl">worth of bitcoin in {' '}
+      <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 bg-black dark:bg-white px-3 py-2 font-semibold text-gray-900 shadow-sm hover:bg-gray-50">
+         { month ?? (<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>)}
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            { months.map(month => (
+              <Menu.Item key={month}>
+              {({ active }) => (
+                <div
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-3xl'
+                  )}
+                  onClick={handleSelectMonth(month)}
+                >
+                  {month}
+                </div>
+              )}
+            </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu> of {/* h3 text */}
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-40 justify-center gap-x-1.5 bg-black dark:bg-white px-3 py-2 font-semibold text-gray-900 shadow-sm hover:bg-gray-50">
+        { year ?? <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> }
+        {/* {year} */}
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-42 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            { years.map(year => (
+              <Menu.Item key={year} >
+              {({ active }) => (
+                <div
+                  onClick={handleSelectYear(year)}
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-3xl'
+                  )}
+                >
+                  {year}
+                </div>
+              )}
+            </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu></h3>
       <svg viewBox={`0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`}>
         <g transform={`translate(${margin.left}, ${margin.top})`}>
           <path d={line(btcPrice)} fill="none" stroke="orange" strokeWidth="2" />
-          <g transform={`translate(0, ${height})`}>
+          {/* <g transform={`translate(0, ${height})`}>
             <g className="x-axis" ref={(node) => d3.select(node).call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y-%m")))} />
             <text
               x={width / 2}
@@ -61,8 +189,8 @@ const bitcoinregrets = ({ btcPrice }) => {
             >
               Date
             </text>
-          </g>
-          <g className="y-axis" ref={(node) => d3.select(node).call(d3.axisLeft(y))} />
+          </g> */}
+          {/* <g className="y-axis" ref={(node) => d3.select(node).call(d3.axisLeft(y))} />
           <text
             //color='red' //'rgb(161,161,170);'
             fill='#a1a1aa'
@@ -73,7 +201,7 @@ const bitcoinregrets = ({ btcPrice }) => {
             dominantBaseline="middle"
           >
             Price (USD)
-          </text>
+          </text> */}
         </g>
       </svg>
     </div>
